ce1f7f9bef2dc33081a067b5b5c57874004c379d
JKRyan <jed@jedkendall.com>
2019-06-24 21:04:31 -0500

    Stashing cookie work for now, will work on other more important aspects

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie");
   res.header("Access-Control-Allow-Methods", "DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", true);
   next();
