80d1f9560a0f3b6b9863fa2073d689f8fb8ac9e8
JKRyan <jed@jedkendall.com>
2019-07-01 20:37:44 -0500

    Changed App component to functional app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import './App.css';
 
@@ -16,37 +16,29 @@ import NewParty from './pages/NewParty';
 
 require("dotenv").config();
 
-class App extends Component {
-  render() {
-    const App = () => (
-      <div className="container light-blue lighten-5 main-app-body">
-        <Nav />
-        <br></br>
-        <div className="row">
-          <Sitepath />
-        </div>
-        <div className="row">
-          <div className="col s12">
-            <Switch>
-              <Route exact path={`/`} component={Home}/>
-              <Route exact path={`/forum`} component={ForumIndex} />
-              <Route exact path={`/forum/:id`} component={Forum}/>
-              <Route exact path={`/forum/:id/thread/:threadid`} component={Thread} />
-              <Route exact path={`/forum/:id/post`} component={NewThread} />
-              <Route exact path={`/parties`} component={Parties} />
-              <Route exact path={`/parties/new`} component={NewParty} />
-              <Route path={`/forum/:id/thread/`} component={NotFound} />
-            </Switch>
-          </div>
-        </div>
+const App = () => (
+  <div className="container light-blue lighten-5 main-app-body">
+    <Nav />
+    <br></br>
+    <div className="row">
+      <Sitepath />
+    </div>
+    <div className="row">
+      <div className="col s12">
+        <Switch>
+          <Route exact path={`/`} component={Home}/>
+          <Route exact path={`/forum`} component={ForumIndex} />
+          <Route exact path={`/forum/:id`} component={Forum}/>
+          <Route exact path={`/forum/:id/thread/:threadid`} component={Thread} />
+          <Route exact path={`/forum/:id/post`} component={NewThread} />
+          <Route exact path={`/parties`} component={Parties} />
+          <Route exact path={`/parties/new`} component={NewParty} />
+          <Route path={`/forum/:id/thread/`} component={NotFound} />
+        </Switch>
       </div>
-    )
-    return (
-      <Switch>
-        <App/>
-      </Switch>
-    );
-  }
-}
+    </div>
+  </div>
+)
+
 
 export default App;
