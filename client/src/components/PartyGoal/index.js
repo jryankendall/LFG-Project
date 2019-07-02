693abad03ac7b7471029450c07c076206e5654f9
JKRyan <jed@jedkendall.com>
2019-07-01 20:14:28 -0500

    8th time's the charm. Come on, heroku.

--- client/src/components/PartyGoal/index.js
+++ client/src/components/PartyGoal/index.js
@@ -1,7 +1,27 @@
 import React, { Component} from 'react';
 
 class PartyGoal extends Component {
-    
+
+    state = {
+        raims: "taims"
+    }
+
+    render() {
+        if (this.props.category === "video games") {
+            return(
+                <div>
+
+                </div>
+            )
+        } else if (this.props.category === "board games") {
+
+        } else if (this.props.category === "other") {
+
+        } else {
+            console.log("What happened?!");
+            
+        };
+    };
 }
 
 export default PartyGoal;
