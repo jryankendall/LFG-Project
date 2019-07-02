324c207bfe9f2a91dac41eb20c95439440f498ff
JKRyan <jed@jedkendall.com>
2019-07-01 20:47:43 -0500

    Flipped router things back

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Nav from './components/Nav';
 import Home from './pages/Home';
@@ -17,27 +16,29 @@ import NewParty from './pages/NewParty';
 require("dotenv").config();
 
 const App = () => (
-  <div className="container light-blue lighten-5 main-app-body">
-    <Nav />
-    <br></br>
-    <div className="row">
-      <Sitepath />
-    </div>
-    <div className="row">
-      <div className="col s12">
-        <Switch>
-          <Route exact path={`/`} component={Home}/>
-          <Route exact path={`/forum`} component={ForumIndex} />
-          <Route exact path={`/forum/:id`} component={Forum}/>
-          <Route exact path={`/forum/:id/thread/:threadid`} component={Thread} />
-          <Route exact path={`/forum/:id/post`} component={NewThread} />
-          <Route exact path={`/parties`} component={Parties} />
-          <Route exact path={`/parties/new`} component={NewParty} />
-          <Route path={`/forum/:id/thread/`} component={NotFound} />
-        </Switch>
+  <Router>
+    <div className="container light-blue lighten-5 main-app-body">
+      <Nav />
+      <br></br>
+      <div className="row">
+        <Sitepath />
+      </div>
+      <div className="row">
+        <div className="col s12">
+          <Switch>
+            <Route exact path={`/`} component={Home}/>
+            <Route exact path={`/forum`} component={ForumIndex} />
+            <Route exact path={`/forum/:id`} component={Forum}/>
+            <Route exact path={`/forum/:id/thread/:threadid`} component={Thread} />
+            <Route exact path={`/forum/:id/post`} component={NewThread} />
+            <Route exact path={`/parties`} component={Parties} />
+            <Route exact path={`/parties/new`} component={NewParty} />
+            <Route path={`/forum/:id/thread/`} component={NotFound} />
+          </Switch>
+        </div>
       </div>
     </div>
-  </div>
+  </Router>
 )
 
 
