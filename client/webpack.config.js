2a2b7cd7c1b6fba05e5c3a04c526ef5c0ece3f70
JKRyan <jed@jedkendall.com>
2019-07-06 10:00:28 -0500

    More webpack experimentation, hopefully this doesn't make this explode.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,31 @@
+const path = require("path");
+
 module.exports = {
     devServer: {
         historyApiFallback: true
     },
-    entry: './src/index.js'
+    node: {
+        fs: "empty"
+    },
+    entry: './src/index.js',
+    output: {
+        path: __dirname,
+        publicPath: './public',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ["@babel/plugin-proposal-class-properties"]
+              }
+            }
+          }
+        ]
+    }
 }
